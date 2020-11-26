ed82edf0fefc91b6bb74452667ad122dc68ed6ed
nick repasy <nrepasy@gmail.com>
2020-11-26 01:16:55 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -12,12 +12,11 @@ end
 
 
 def selection(num)
-  if num = 1
-  
-     "YUM YUM MUNCH MUNCH MUNCH"
+  if num = 1 
+    puts "YUM YUM MUNCH MUNCH MUNCH"
   elsif num = 2
 
-     "HAM HAM HAM IN MY TUMMY" 
+    puts "HAM HAM HAM IN MY TUMMY" 
 end
 end
 
